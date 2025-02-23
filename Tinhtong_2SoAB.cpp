82896d7d49bd9014350b47b382ea2dbe062578b0
TruongNguQuyen <quyen.tn.64cntt@ntu.edu.vn>
2025-02-23 14:08:23 +0700

    New update
    Them bai tap tinh tong 2 so a va b

---
+++ Tinhtong_2SoAB.cpp
@@ -0,0 +1,20 @@
+// khai bao thu vien
+#include <iostream>
+
+using namespace std;
+
+// Khai bao ham main de chay chuong trinh
+int main(){
+	// Chuong trinh tinh tong 2 so a va b
+	
+	// Khoi tao 2 so a va b
+	int a, b;
+	
+	// Nhap du lieu cho 2 so a va b
+	cin >> a;
+	cin >> b;
+	
+	// Tinh tong cho 2 so a va b
+	cout << a + b;
+	return 0;
+}
